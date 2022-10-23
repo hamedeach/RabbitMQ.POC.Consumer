bfce4cc219fc6f72dc3b21602bef0662f21439df
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-10-23 20:11:30 +0200

    consumer /routing

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ using System.Threading;
 
 Console.WriteLine("######### RabbitMQ Consumer APP ############");
 
+Random random = new Random();
+string instanceID = RandomString(4);
+Console.WriteLine($"######### ID : {instanceID} ############");
 
 var factory = new ConnectionFactory { HostName = "localhost" };
 var connection = factory.CreateConnection();
@@ -16,8 +19,10 @@ using var channel = connection.CreateModel();
 
 // define the exchange 
 channel.ExchangeDeclare(exchange: "myroutingexchange", ExchangeType.Direct);
-
+//define the queue
 var queueName = channel.QueueDeclare().QueueName;
+// bind the queue to the exchange 
+channel.QueueBind(queue: queueName, exchange: "myroutingexchange", routingKey: $"{instanceID}_BindingKey");
 
 var consumer = new EventingBasicConsumer(channel);
 consumer.Received += Consumer_Received;
@@ -26,14 +31,21 @@ void Consumer_Received(object? sender, BasicDeliverEventArgs e)
 {
     
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
-    Console.WriteLine($"Consumner Received {message}");
+    Console.WriteLine($"{instanceID} - Consumner Received {message}");
 }
 
 while (true)
 {
     Thread.Sleep(1000);
-    channel.BasicConsume(queue: "echoQ",
+    channel.BasicConsume(queue: queueName,
     autoAck: true,
     consumer: consumer);
 
+}
+
+string RandomString(int length)
+{
+    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    return new string(Enumerable.Repeat(chars, length)
+        .Select(s => s[random.Next(s.Length)]).ToArray());
 }
