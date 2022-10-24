90c5e1028384d21fabf77d502552e6f2953143f7
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-10-24 16:04:57 +0200

    finish the example

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 
-Console.WriteLine("######### RabbitMQ Consumer APP ############");
+Console.WriteLine("######### RabbitMQ Client APP ############");
 
 
 var factory = new ConnectionFactory { HostName = "localhost" };
@@ -14,27 +14,53 @@ var connection = factory.CreateConnection();
 
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "echoQ",
-    durable: false,
-    exclusive: false,
-    autoDelete: false,
-    arguments: null);
+// declare the replay queue where the client will receive the response
+var replyQueue = channel.QueueDeclare(queue: "", exclusive: true);
+
+//declare the requests queue where the client will send the request to the server the serever will consume this queue
+var requestQueue = channel.QueueDeclare(queue: "request-queue", exclusive: false);
+
 
 var consumer = new EventingBasicConsumer(channel);
 consumer.Received += Consumer_Received;
 
 void Consumer_Received(object? sender, BasicDeliverEventArgs e)
 {
-    
+
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
-    Console.WriteLine($"Consumner Received {message}");
+    Console.WriteLine($"the client Received the replay to the request id {e.BasicProperties.CorrelationId} {message}");
 }
 
-while (true)
-{
-    Thread.Sleep(1000);
-    channel.BasicConsume(queue: "echoQ",
+//consume the queue from the server 
+channel.BasicConsume(queue: replyQueue,
     autoAck: true,
     consumer: consumer);
 
+while (true)
+{
+    Task.Delay(2500).Wait();    
+    // define a request to the client
+    var requestMessage = "What is the current time ?";
+    var requestBody = Encoding.UTF8.GetBytes(requestMessage);
+
+    var properties = channel.CreateBasicProperties();
+    properties.ReplyTo = replyQueue.QueueName;
+    properties.CorrelationId = Guid.NewGuid().ToString();
+    //publish the request to the server
+    channel.BasicPublish("", "request-queue", properties, requestBody);
+    Console.WriteLine($"the client sending a request with id {properties.CorrelationId}");
+
 }
+
+//while (true)
+//{
+//    Thread.Sleep(1000);
+//    channel.BasicConsume(queue: replyQueue,
+//    autoAck: true,
+//    consumer: consumer);
+
+//}
+
+
+
+
